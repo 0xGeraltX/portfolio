7b2dd8c5625b2cafe5983c633e81515e0d438069
Geralt | Web3 <cryptonsorav@gmail.com>
2025-12-24 14:58:49 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
-
-export default function Home(){return(<><Navbar/><Hero/><Skills/><Projects/><Contact/></>)}
